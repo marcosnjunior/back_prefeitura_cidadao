fe03c6b2f90e817d9b79a114b5734a96ae59ea37
Thiago Mariano <134088653+ThiagoMarianoDeveloper@users.noreply.github.com>
2024-08-09 19:58:02 +0000

    salvar

--- prefeitura/eventos/views.py
+++ prefeitura/eventos/views.py
@@ -14,3 +14,39 @@ def familia_acolhedora(request):
 
 #resultado = Fornecedor.objects.all()
     #return HttpResponse(resultado)
+
+
+
+
+    
+"""
+def home(request):
+    return render(request, 'loja/home.html', {})
+
+
+def cadastrar(request):
+
+    if request.method == "POST":
+
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco')
+        quantidade = request.POST.get('quantidade')
+
+        novo_produto = Produto()
+        novo_produto.nome = nome
+        novo_produto.preco = preco
+        novo_produto.quantidade = quantidade
+        novo_produto.save()
+
+    return render(request, 'loja/cadastrar.html', {})
+
+
+
+def pesquisar(request):
+
+
+    if request.method == "POST":
+        pesquisa = request.POST.get('pesquisa')
+        resultado = Produto.objects.filter(nome__contains=pesquisa) 
+        return render(request,'loja/pesquisa.html',{'nome': 'Thiago', 'resultado':resultado})
+"""
