dc3ec44173d08b5d6802a517a486f6ef2b5265fc
RianDantas <134387076+RianDantas@users.noreply.github.com>
2024-08-15 18:47:12 +0000

    nbhj

--- prefeitura/meio_ambiente/forms.py
+++ prefeitura/meio_ambiente/forms.py
@@ -6,5 +6,13 @@ class Plantio_de_ArvoreForm(forms.ModelForm):
     class Meta:
         model = Plantio_de_Arvores
         fields ='__all__'
-        
+        labels = {
+            'larg_compr_calcada' : 'Largura X Comprimento da Calçada',
+            'observacoes' : 'Observações',
+            'cep' : 'CEP',
+            'bairro' : 'Bairro',
+            'nome_rua' : 'Nome da Rua',
+            'ponto_refer' : 'Ponto de Referência'
+        }
+
         
