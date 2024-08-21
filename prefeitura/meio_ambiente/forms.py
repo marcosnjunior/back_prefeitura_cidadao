196e6662838fbad596704ff3b6e2dcb089fddd7b
RianDantas <134387076+RianDantas@users.noreply.github.com>
2024-08-21 03:01:40 +0000

    Finalizado

--- prefeitura/meio_ambiente/forms.py
+++ prefeitura/meio_ambiente/forms.py
@@ -7,12 +7,21 @@ class Plantio_de_ArvoreForm(forms.ModelForm):
         model = Plantio_de_Arvores
         fields ='__all__'
         labels = {
-            'larg_compr_calcada' : 'Largura X Comprimento da Calçada',
+            'larg_compr_calcada' : 'Largura X Comprimento da Calçada *',
             'observacoes' : 'Observações',
-            'cep' : 'CEP',
-            'bairro' : 'Bairro',
-            'nome_rua' : 'Nome da Rua',
-            'ponto_refer' : 'Ponto de Referência'
+            'cep' : 'CEP *',
+            'bairro' : 'Bairro *',
+            'nome_rua' : 'Nome da Rua *',
+            'ponto_refer' : 'Ponto de Referência *'
+        }
+
+        widgets = {
+            'larg_compr_calcada': forms.TextInput(attrs={'placeholder': '   Largura X Comprimento da sua calçada'}),
+            'observacoes': forms.Textarea(attrs={'placeholder': '   Observações'}),
+            'cep': forms.TextInput(attrs={'placeholder': '   CEP'}),
+            'bairro': forms.TextInput(attrs={'placeholder': '   Bairro:'}),
+            'nome_rua': forms.TextInput(attrs={'placeholder': '   Nome da rua'}),
+            'ponto_refer': forms.TextInput(attrs={'placeholder': '   Ponto de Referência:'}),
         }
 
 class Poda_de_ArvoresForm(forms.ModelForm):
@@ -31,6 +40,20 @@ class Poda_de_ArvoresForm(forms.ModelForm):
             'telefone' : 'Telefone'
         }
 
+        widgets = {
+            'rua' : forms.TextInput(attrs={'placeholder' : '   Rua'}),
+            'numero' : forms.TextInput(attrs={'placeholder' : '   Número'}),
+            'bairro' : forms.TextInput(attrs={'placeholder' : '   Bairro'}),
+            'estado' : forms.TextInput(attrs={'placeholder' : '   Estado'}),
+            'cidade' : forms.TextInput(attrs={'placeholder' : '   Cidade'}),
+            'cep' : forms.TextInput(attrs={'placeholder' : '   CEP'}),
+            'ponto_refer' : forms.TextInput(attrs={'placeholder' : '   Ponto de Referência'}),
+            'observacoes': forms.Textarea(attrs={'placeholder': '   Observações'}),
+            'telefone' : forms.TextInput(attrs={'placeholder' : '   Telefone'}),
+        }
+            
+        
+
 class lancamento_de_agua_servidasForm(forms.ModelForm):
     class Meta:  
         model = lancamento_de_agua_servidas
@@ -41,6 +64,14 @@ class lancamento_de_agua_servidasForm(forms.ModelForm):
             'rua_problema' : 'Insira a Rua do local do problema *',
             'ponto_refer' : 'Ponto de referência: *',
             'observacoes' : 'Observações'
+        }
+
+        widgets = {
+            'cep_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o CEP Do local do Problema'}),
+            'bairro_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o Bairro Do local do Problema'}),
+            'rua_problema' : forms.TextInput(attrs={'placeholder' : '   Insira A Rua Do local do Problema'}),
+            'ponto_refer' : forms.TextInput(attrs={'placeholder' : '   Ponto de referência'}),
+            'observacoes': forms.Textarea(attrs={'placeholder': '   Observações'}),
         }
 
 class lancamento_esgotoForm(forms.ModelForm):
@@ -55,6 +86,15 @@ class lancamento_esgotoForm(forms.ModelForm):
             'observacoes' : 'Observações'
         }
 
+        widgets = {
+            'cep_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o CEP Do local do Problema'}),
+            'bairro_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o Bairro Do local do Problema'}),
+            'rua_problema' : forms.TextInput(attrs={'placeholder' : '   Insira A Rua Do local do Problema'}),
+            'ponto_refer' : forms.TextInput(attrs={'placeholder' : '   Ponto de referência'}),
+            'observacoes': forms.Textarea(attrs={'placeholder': '   Observações'}),
+        }
+
+
 class poluicao_sonoraForm(forms.ModelForm):
     class Meta:  
         model = poluicao_sonora
@@ -67,6 +107,15 @@ class poluicao_sonoraForm(forms.ModelForm):
             'ponto_refer' : 'Ponto de referência: *'
         }
 
+        widgets = {
+            'observacoes': forms.Textarea(attrs={'placeholder': '   Observações'}),
+            'cep_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o CEP Do local do Problema'}),
+            'bairro_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o Bairro Do local do Problema'}),
+            'rua_problema' : forms.TextInput(attrs={'placeholder' : '   Insira A Rua Do local do Problema'}),
+            'ponto_refer' : forms.TextInput(attrs={'placeholder' : '   Ponto de referência'}),
+        }
+
+
 class corte_irregular_arvoresForm(forms.ModelForm):
     class Meta:  
         model = corte_irregular_arvores
@@ -77,6 +126,14 @@ class corte_irregular_arvoresForm(forms.ModelForm):
             'bairro_problema' : 'Insira o Bairro do Problema*',
             'rua_problema' : 'Insira a Rua do local do problema *',
             'ponto_refer' : 'Ponto de referência: *'
+        }
+
+        widgets = {
+            'observacoes': forms.Textarea(attrs={'placeholder': '   Observações'}),
+            'cep_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o CEP Do local do Problema'}),
+            'bairro_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o Bairro Do local do Problema'}),
+            'rua_problema' : forms.TextInput(attrs={'placeholder' : '   Insira A Rua Do local do Problema'}),
+            'ponto_refer' : forms.TextInput(attrs={'placeholder' : '   Ponto de referência'}),
         }
 
 class poluicao_atmosfericaForm(forms.ModelForm):
@@ -91,6 +148,14 @@ class poluicao_atmosfericaForm(forms.ModelForm):
             'ponto_refer' : 'Ponto de referência: *'
         }
 
+        widgets = {
+            'observacoes': forms.Textarea(attrs={'placeholder': '   Observações'}),
+            'cep_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o CEP Do local do Problema'}),
+            'bairro_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o Bairro Do local do Problema'}),
+            'rua_problema' : forms.TextInput(attrs={'placeholder' : '   Insira A Rua Do local do Problema'}),
+            'ponto_refer' : forms.TextInput(attrs={'placeholder' : '   Ponto de referência'}),
+        }
+
 class invasao_area_preservacao_permanenteForm(forms.ModelForm):
     class Meta:  
         model = invasao_area_preservacao_permanente
@@ -101,6 +166,14 @@ class invasao_area_preservacao_permanenteForm(forms.ModelForm):
             'bairro_problema' : 'Insira o Bairro do Problema*',
             'rua_problema' : 'Insira a Rua do local do problema *',
             'ponto_refer' : 'Ponto de referência: *'
+        }
+
+        widgets = {
+            'observacoes': forms.Textarea(attrs={'placeholder': '   Observações'}),
+            'cep_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o CEP Do local do Problema'}),
+            'bairro_problema' : forms.TextInput(attrs={'placeholder' : '   Insira o Bairro Do local do Problema'}),
+            'rua_problema' : forms.TextInput(attrs={'placeholder' : '   Insira A Rua Do local do Problema'}),
+            'ponto_refer' : forms.TextInput(attrs={'placeholder' : '   Ponto de referência'}),
         }
 
             
