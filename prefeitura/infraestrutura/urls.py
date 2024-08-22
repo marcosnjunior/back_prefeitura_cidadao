df28ec4e9bb7bd2c98317ea6bdae75358b4e856a
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-08-22 13:57:12 -0300

    mark10

--- prefeitura/infraestrutura/urls.py
+++ prefeitura/infraestrutura/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("",views.home),
     path("problema_iluminacao",views.problema_iluminacao),
     path("expansao_iluminacao",views.expansao_iluminacao),
+    path("manutencaoVias",views.manutencao),
+    path("manutencaoVias/tapa_buraco",views.tapa_buraco),
+    path("manutencaoVias/galeria_boca_lobo",views.tapa_buraco),
+    path("manutencaoVias/terraplanagem",views.tapa_buraco),
 ]
--- prefeitura/infraestrutura/views.py
+++ prefeitura/infraestrutura/views.py
@@ -4,6 +4,10 @@ from .forms import *
 def home(request):
     return render(request,'iluminacao.html')
 
+def manutencao(request):
+    return render(request,'manutencaoVias.html')
+
+
 
 def problema_iluminacao(request):
     if request.method == 'POST':
@@ -14,7 +18,7 @@ def problema_iluminacao(request):
             return redirect('/infraestrutura')
     else:
         form = Problema_iluminacaoForm()
-    return render(request, 'problema_iluminacao.html', {'form': form})
+    return render(request, 'iluminacao/problema_iluminacao.html', {'form': form})
 
 def expansao_iluminacao(request):
     if request.method == 'POST':
@@ -25,6 +29,42 @@ def expansao_iluminacao(request):
             return redirect('infraestrutura')
     else:
         form = Expansao_iluminacaoForm()
-    return render(request, 'expansao_iluminacao.html', {'form': form})
+    return render(request, 'iluminacao/expansao_iluminacao.html', {'form': form})
+
+
+def tapa_buraco(request):
+    if request.method == 'POST':
+        form = Tapa_buracoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form.clean()
+            return redirect('infraestrutura')
+    else:
+        form = Tapa_buracoForm()
+    return render(request, 'moblidadeUrbana/tapa_buraco.html', {'form': form})
+
+
+def galeria_boca_lobo(request):
+    if request.method == 'POST':
+        form = GaleriaBocaLoboForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form.clean()
+            return redirect('infraestrutura')
+    else:
+        form = GaleriaBocaLoboForm()
+    return render(request, 'moblidadeUrbana/galeria_boca_lobo.html', {'form': form})
+
+
+def terraplanagem(request):
+    if request.method == 'POST':
+        form = TerraplanagemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form.clean()
+            return redirect('infraestrutura')
+    else:
+        form = TerraplanagemForm()
+    return render(request, 'moblidadeUrbana/terraplanagem.html', {'form': form})
 
 
